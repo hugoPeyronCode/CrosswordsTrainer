55952b74fdeb22a46c3fc12fddec87717da31bef
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-04-11 17:07:12 +0200

    Initial Commit

---
+++ CrosswordsTrainer/CrosswordsTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrosswordsTrainerApp.swift
+//  CrosswordsTrainer
+//
+//  Created by Hugo Peyron on 11/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CrosswordsTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
